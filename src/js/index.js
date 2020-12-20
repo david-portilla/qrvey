f274c1d9834b8b647658fd053361327d5970cc9a
David Portilla <david_portilla@hotmail.com>
2020-12-20 14:17:55 -0500

    js entry point

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import '../scss/styles.scss'
+import { selectId } from './helpers'
+import Fetch from './components/Fetch'
+
+console.log('index.js') // add custom select
+
+if (selectId('listContainer')) {
+  /* eslint-disable-next-line */
+  let data = new Fetch()
+}
