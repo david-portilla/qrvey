963c073d1499228e9f1d69c25b8d7ed4a3feb8ff
David Portilla <david_portilla@hotmail.com>
2020-12-20 14:46:22 -0500

    add Fetch component

---
+++ src/js/components/Fetch.js
@@ -0,0 +1,68 @@
+import {selectId} from '../helpers'
+import Draw from './Draw'
+
+/**
+* Handle fetch comunication
+* @author David Portilla <david_portilla@hotmail.com>
+*/
+
+class Fetch {
+  /**
+ * Constructor method
+ */
+  constructor () {
+    this.loading = selectId('loading')
+    this.showLoading = this.showLoading.bind(this)
+    this.init()
+  }
+
+  /**
+* Initialize method
+*/
+  init () {
+    this.fetchItem('all')
+    this.showLoading(true)
+  }
+
+  /**
+* Pull data data from https://restcountries.eu/
+* @param {string} id the parameter for pulling the data
+* @returns {solved Promise}
+*/
+  async fetchItem (id) {
+    try {
+      const params = {
+        method: 'GET',
+        mode: 'cors'
+      }
+      const url = `https://restcountries.eu/rest/v2/${ id }?fields=region;name;flag;capital;population;currencies;languages;borders`
+      // const url = `https://restcountries.eu/rest/v2/${ id }/`
+      /* eslint-disable-next-line */
+      const response = await fetch(url, params)
+      if (response.ok) {
+        const data = await response.json()
+
+        if (data) {
+          this.showLoading(false)
+          /* eslint-disable-next-line */
+          let sendData = new Draw({data})
+        }
+      }
+    } catch (error) {
+      console.log('Fetch Item Failed: ', error.message)
+    }
+  }
+
+  /**
+* Show loading animation
+* @param {boolean} bol - The boolena to control dots animation
+*/
+  showLoading (bol) {
+    if (bol === true) {
+      this.loading.classList.add('show')
+    } else {
+      this.loading.classList.remove('show')
+    }
+  }
+}
+export default Fetch
