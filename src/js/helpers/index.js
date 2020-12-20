b21163738053fce240592ba35b02d65c21c10aca
David Portilla <david_portilla@hotmail.com>
2020-12-20 14:24:06 -0500

    adding helpers  for common query selectors

---
+++ src/js/helpers/index.js
@@ -0,0 +1,4 @@
+export var select = document.querySelector.bind(document)
+export var selectAll = document.querySelectorAll.bind(document)
+export var selectId = document.getElementById.bind(document)
+export var selectTagName = document.getElementsByName.bind(document)
