df0067b2406de6c4711b5270473c12affa422a06
David Portilla <david_portilla@hotmail.com>
2020-12-21 10:30:07 -0500

    update Search functionality

--- src/js/components/Search.js
+++ src/js/components/Search.js
@@ -1,11 +1,6 @@
-// import {select, selectId} from '../helpers'
-
 import {selectAll, selectId} from '../helpers'
-
 class Search {
-
   constructor () {
-    console.log('Search.js')
     this.searchBar = selectId('get-country')
     this.listItems = selectAll('.c-list__item')
     // bind methods
@@ -14,11 +9,10 @@ class Search {
     this.init()
   }
 
-
   init () {
     this.searchBar.addEventListener('input', (e) => {
-      this.filterData(e.target.value);
-    });
+      this.filterData(e.target.value)
+    })
   }
 
   filterData (searchTerm) {
@@ -30,6 +24,6 @@ class Search {
       }
     })
   }
-
 }
+
 export default Search
